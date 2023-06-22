37f862a35a89e7124df31c10ea96a4aeb5f46243
Shuwen He <1201220707@pku.edu.cn>
2023-06-22 20:03:35 +0800

    fix: pow fstream if

--- code/csp-j.cpp
+++ code/csp-j.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -235,8 +236,26 @@ int logicalOperator(){
 }
 
 
-
 int main() {
-	logicalOperator();
-}
+    ifstream fin("pow.in");
+    ofstream fout("pow.out");
 
+    int a, b;
+    fin >> a >> b;
+
+    long long result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
+        if (result > 1000000000) {
+            fout << -1 << endl;
+            return 0;
+        }
+    }
+
+    fout << result << endl;
+
+    fin.close();
+    fout.close();
+
+    return 0;
+}
