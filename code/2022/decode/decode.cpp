f68d41c701973dca3547395a5395a7b9d1a79420
Shuwen He <1201220707@pku.edu.cn>
2023-06-27 09:17:18 +0800

    fix: pow decode

--- code/2022/decode/decode.cpp
+++ code/2022/decode/decode.cpp
@@ -3,31 +3,6 @@
 
 using namespace std;
 
-// 2022CSP-J2-pow乘方
-int pow() {
-    ifstream fin("pow.in");
-    ofstream fout("pow.out");
-
-    int a, b;
-    fin >> a >> b;
-
-    long long result = 1;
-    for (int i = 0; i < b; i++) {
-        result *= a;
-        if (result > 1000000000) {
-            fout << -1 << endl;
-            return 0;
-        }
-    }
-
-    fout << result << endl;
-
-    fin.close();
-    fout.close();
-
-    return 0;
-}
-
 // 2022CSP-J2-decode解密
 bool isPrime(int num) {
     if (num < 2)
