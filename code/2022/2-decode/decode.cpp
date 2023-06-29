5e6354e5576963c6e54b3bdd5311cbba9be27690
Shuwen He <1201220707@pku.edu.cn>
2023-06-29 16:36:41 +0800

    fix: decode

--- code/2022/2-decode/decode.cpp
+++ code/2022/2-decode/decode.cpp
@@ -3,55 +3,30 @@
 
 using namespace std;
 
-bool isPrime(int num) {
-    if (num < 2)
-        return false;
-    for (int i = 2; i * i <= num; ++i) {
-        if (num % i == 0)
-            return false;
-    }
-    return true;
-}
-
-int decode() {
-    ifstream inputFile("decode.in");
-    ofstream outputFile("decode.out");
-
-    int k;
-    inputFile >> k;
-
-    for (int i = 0; i < k; ++i) {
-        int n, d, e;
-        inputFile >> n >> d >> e;
-
-        int p = -1, q = -1;
-        for (int j = 2; j * j <= n; ++j) {
-            if (n % j == 0 && isPrime(j)) {
-                p = j;
-                break;
-            }
-        }
-        if (p == -1) {
-            outputFile << "NO\n";
-            continue;
-        }
-        q = n / p;
-
-        if ((p - 1) * (q - 1) + 1 != e * d) {
-            outputFile << "NO\n";
-            continue;
-        }
-
-        outputFile<<i + 1 << p << " " << q << "\n";
-    }
-
-    inputFile.close();
-    outputFile.close();
-
-    return 0;
-}
-
 int main(){
-	decode();
+	ifstream fin("decode.in");
+	ofstream fout("decode.out");
+	int k;
+	fin>>k;
+	for(int i = 0; i < k;i++){
+		long long n,d,e;
+		bool found = false;
+		for(long long p = 2;p * p <= n;p++){
+			if (n % p == 0){
+				long long q = n / p;
+				if ((p - 1)*(q - 1) + 1 == d * e ){
+					fout<<p<<""<<q<<endl;
+					found = true;
+					break;
+				}
+			}
+
+		}
+		if (!found){
+			fout<<"NO"<<endl;
+		}
+	}
+	fin.close();
+	fout.close();
 	return 0;
 }
