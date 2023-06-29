e73b2ce3b5a4ffad2a8a1230f4e2cd39432422e0
Shuwen He <1201220707@pku.edu.cn>
2023-06-29 17:21:16 +0800

    fix: decode

--- code/2022/2-decode/decode.cpp
+++ code/2022/2-decode/decode.cpp
@@ -3,30 +3,48 @@
 
 using namespace std;
 
-int main(){
-	ifstream fin("decode.in");
-	ofstream fout("decode.out");
-	int k;
-	fin>>k;
-	for(int i = 0; i < k;i++){
-		long long n,d,e;
-		bool found = false;
-		for(long long p = 2;p * p <= n;p++){
-			if (n % p == 0){
-				long long q = n / p;
-				if ((p - 1)*(q - 1) + 1 == d * e ){
-					fout<<p<<""<<q<<endl;
-					found = true;
-					break;
-				}
-			}
+// 判断一个数是否为素数
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-		}
-		if (!found){
-			fout<<"NO"<<endl;
-		}
-	}
-	fin.close();
-	fout.close();
-	return 0;
+// 解密函数
+void decode(int n, int d, int e) {
+    for (int p = 2; p < n; p++) {
+        if (n % p == 0 && isPrime(p)) {
+            int q = n / p;
+            int phi = (p - 1) * (q - 1);
+            if ((e * d) % phi == 1) {
+                cout << p << " " << q << endl;
+                return;
+            }
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int main() {
+    int k;
+    ifstream input("decode.in");
+    ofstream output("decode.out");
+
+    input >> k;
+    for (int i = 0; i < k; i++) {
+        int n, d, e;
+        input >> n >> d >> e;
+        decode(n, d, e);
+    }
+
+    input.close();
+    output.close();
+
+    return 0;
 }
