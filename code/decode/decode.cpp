0e0869a30fdbf6e74c9ed6ab96befec4cfa72fcf
Shuwen He <1201220707@pku.edu.cn>
2023-07-20 11:56:51 +0800

    feat: binary search

--- code/decode/decode.cpp
+++ code/decode/decode.cpp
@@ -12,17 +12,28 @@ int main(){
 		long long n,d,e;
 		fin>>n>>d>>e;
 		long long s = n - d * e + 2;
-		long long l = 1,r = s - 1;
+		long long left = 1,right = s - 1;
 		long long p = -1, q = -1;
 
-		while(l <= r){
-			long long mid  = l + (r - l) / 2;
+		while(left <= right){
+			long long mid  = left + (right - left) / 2;
 			if(mid * (s - mid) == n){
 				p = mid;
-
-
+				q = s - mid;	
+				break;
+			}else if(mid*(s-mid)<n){
+				left = mid + 1;
+			}else{
+				right = mid - 1;
 			}
 		}
-
+		if(p != -1 && q != -1 && p <= q){
+			fout<<p<<" "<<q<<endl;
+		}else{
+			fout<<"NO"<<endl;
+		}
 	}
+	fin.close();
+	fout.close();
+	return 0;
 }
