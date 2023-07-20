6eaa6aaa7ca8dcd8bac021e17d1dccfa8e392a80
Shuwen He <1201220707@pku.edu.cn>
2023-07-20 09:34:37 +0800

    feat: factorial

--- code/binary-search/binary-search.cpp
+++ code/binary-search/binary-search.cpp
@@ -5,27 +5,25 @@
 using namespace std;
 
 int binarySearch(vector<int>& nums,int target){
-int left = 0;
-int right = nums.size()-1;
-int mid = left + (right - left)/2;
-while(left < right){
-if(nums[mid] == target){
-return mid;
-}else if(nums[mid] < target){
-left = mid + 1;
-}else{
-right = mid - 1;
-}
-}
-return -1;
+	int mid;
+	int left = 0,right = nums.size()-1;
+	mid = left+(right - left)/2; 
+	if(nums[mid] == target){
+		return mid;
+	}else if(nums[mid] < target){
+		left = mid + 1;
+	}else{
+		right = mid - 1;
+	}
+	return -1;
 }
 
 int main(){
-ifstream fin("binary.in");
-ofstream fout("binary.out");
-vector<int> nums = {1,3,5,7,9,11,13,15};
-int target;
-fin>>target;
-fout<<binarySearch(nums,target);
-return 0;
+	ifstream fin("binary.in");
+	ofstream fout("binary.out");
+	vector<int> nums = {1,3,5,7,9,11,13,15};
+	int target;
+	fin>>target;
+	fout<<binarySearch(nums,target)<<endl;
+	return 0;
 }
--- code/factorial/factorial.cpp
+++ code/factorial/factorial.cpp
@@ -4,21 +4,20 @@
 using namespace std;
 
 long long factorial(int num){
-long long result = 1;
-for(int i = 1; i <= num; i++){
-result *= i;
-}
-return result;
+	long long result = 1;
+	for(int i = 2; i <= num; i++){
+		result *= i;
+	}
+	return result;
 }
 
 int main(){
-ifstream fin("factorial.in");
-ofstream fout("factorial.out");
-
-int num;
-long long result;
-fin>>num;
-result = factorial(num);
-fout>>result;
-return 0;
+	ifstream fin("factorial.in");
+	ofstream fout("factorial.out");
+	int num;
+	long long result;
+	fin>>num;
+	result = factorial(num);
+	fout << result;
+	return 0;
 }
