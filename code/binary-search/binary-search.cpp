1b20a29a4e0ae12db1a878e066e08ba15d81434c
Shuwen He <1201220707@pku.edu.cn>
2023-07-20 22:44:14 +0800

    feat: binary search

--- code/binary-search/binary-search.cpp
+++ code/binary-search/binary-search.cpp
@@ -5,28 +5,32 @@
 using namespace std;
 
 int binarySearch(vector<int>& nums,int target){
-	int left = 0;
-	int right = nums.size()-1;
-
+	int left = 0,right = nums.size()-1;
 	while(left <= right){
-	int mid = left+(right - left)/2; 
-	if(nums[mid] == target){
-		return mid;
-	}else if(nums[mid] < target){
-		left = mid + 1;
-	}else{
-		right = mid - 1;
+		int mid = left + (right - left)/2;
+		if(nums[mid] == target){
+			return mid;
+		}else if(nums[mid] < target){
+			left = mid + 1;
+		}else{
+			right = mid - 1;
 		}
 	}
 	return -1;
 }
 
-int main(){
+int main(){	
 	ifstream fin("binary.in");
 	ofstream fout("binary.out");
-	vector<int> nums = {1,3,5,7,9,11,13,15};
+
+	vector<int> nums = {1,3,5,7,9};
 	int target;
 	fin>>target;
-	fout<<binarySearch(nums,target)<<endl;
+
+	int n = binarySearch(nums,target);
+	fout<<n;
+	fin.close();
+	fout.close();
+	
 	return 0;
 }
