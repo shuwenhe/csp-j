44a2aa93154acf3d939a8009d326c9cec8f23dda
Shuwen He <1201220707@pku.edu.cn>
2023-07-23 14:58:28 +0800

    feat: decode

--- code/number/number.cpp
+++ code/number/number.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
 	ifstream fin("number.in");
 	ofstream fout("number.out");
+	
+	string str;
+	fin>>str;
+
+	long count = 0;
+	for(long i = 0; i < str.length(); i++){
+		if(str[i] == '1'){
+			count++;
+		}
+	}
+	fout<<count;
+	fin.close();
+	fout.close();
+	return 0;
 }
