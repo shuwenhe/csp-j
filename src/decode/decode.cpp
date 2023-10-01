c45d6389f844df39031c08e2771291c1a8bc7813
Shuwen He <1201220707@pku.edu.cn>
2023-10-01 22:54:12 +0800

    feat: number

--- src/decode/decode.cpp
+++ src/decode/decode.cpp
@@ -1,40 +1,34 @@
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
 	ifstream fin("decode.in");
 	ofstream fout("decode.out");
-
-	long long k;
+	int k;
 	fin>>k;
-	for(long long i = 1;i <= k; i++ ){
+	for(int i = 0; i < k; i++){
 		long long n,d,e;
 		fin>>n>>d>>e;
-		long long s;
-		s = n - d*e +2;
-		long long left = 1,right = s-1;
+		long long s = n - d * e + 2;
 		long long p = -1,q = -1;
+		long long left = 1, right = s - 1;
 		while(left <= right){
-			long long mid = left + (right - left)/2;
-			if(mid*(s-mid)==n){
+			long long mid = left + (right - left) / 2;
+			if(mid * (s - mid) == n){
 				p = mid;
 				q = s - mid;
 				break;
-			}else if(mid*(s - mid)<n){
+			}else if(mid * (s - mid) < n){
 				left = mid + 1;
-			}else{
+			}else {
 				right = mid - 1;
 			}
 		}
-		if(p != -1 && q != -1 && p<q){
-			fout<<p<<" "<<q<<endl;
+		if(p != -1 && q != -1 && p <= q){
+			fout<<p<<' '<<q<<endl;
 		}else{
 			fout<<"NO"<<endl;
 		}
 	}
-	fin.close();
-	fout.close();
-	return 0;
 }
