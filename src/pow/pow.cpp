2fdeccd7c576c8729e93e6031c5e1c79d6c438a3
Shuwen He <1201220707@pku.edu.cn>
2023-10-01 22:13:28 +0800

    feat: pow

--- src/pow/pow.cpp
+++ src/pow/pow.cpp
@@ -1,27 +1,14 @@
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
 	ifstream fin("pow.in");
 	ofstream fout("pow.out");
-	
-	long long a,b;
+	int a,b;
 	fin>>a>>b;
+	cout<<a<<b;
 
-	long long result = 1;
-	for(long i = 0; i < b; i++){
-		result *= a;
-		if(result > 1e9){
-			fout<<-1;
-			return 0;
-		}
-	}
-	fout<<result;
-
-	fin.close();
-	fout.close();
 
 	return 0;
 }
